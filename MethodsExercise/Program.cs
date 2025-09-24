99364922fd75d105c75af8ad68a6f5aa2967f9e7
Joshua Dela Pena <shiroganebigboi@gmail.com>
2025-09-24 13:20:10 -0500

    Finished creating c# story program (exercise 1)

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,29 @@
 {
     public class Program
     {
+        public static void littleIntro()
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}! WHat's your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("What's your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("What's your favorite band?");
+            string favoriteBand = Console.ReadLine();
+            
+            Console.WriteLine($"{userName} adores {favoriteAnimal} especially {favoriteColor} " +
+                              $"colored ones. {userName}'s favorite hobby is to listen to their " +
+                              $"favorite band {favoriteBand}.");
+        }
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            littleIntro();
+            
+            
         }
+        
     }
 }
