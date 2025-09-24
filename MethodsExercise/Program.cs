77bca175dd633ba86f7a6310ceddcc9a7b2a694e
Joshua Dela Pena <shiroganebigboi@gmail.com>
2025-09-24 13:26:06 -0500

    Completed math operation methods (exercise 2)

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -18,11 +18,33 @@
                               $"favorite band {favoriteBand}.");
         }
 
-        
+        public static int Add(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public static int Multiply(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+
+        public static int Divide(int numberOne, int numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+
+
+        public static int Subtract(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
         static void Main(string[] args)
         {
-            littleIntro();
-            
+            //littleIntro();
+            Console.WriteLine(Add(numberOne:5, numberTwo:5));
+            Console.WriteLine(Multiply(numberOne:5, numberTwo:4));
+            Console.WriteLine(Divide(numberOne:10, numberTwo:5));
+            Console.WriteLine(Subtract(numberOne:20, numberTwo:20));
             
         }
         
